a84118e65e946c629fad50f15f880c7b832b225e
Micky-ragazzo <w48778@student.wsiz.rzeszow.pl>
2019-07-23 06:31:20 +0200

    commit initial

--- script.R
+++ script.R
@@ -94,68 +94,137 @@ eng.5. <- list.files(pattern = "eng.5.csv", recursive = TRUE) %>%
   lapply(read_csv) %>% 
   bind_rows 
 
+eng.3a. <- list.files(pattern = "eng.3a.csv", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+eng.3b. <- list.files(pattern = "eng.3b.csv", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+eng <- list.files(pattern = ".*(eng).*csv$", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+bel <- list.files(pattern = "1-proleague.csv", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+# de.1. <- list.files(pattern = "de.1.csv", recursive = TRUE) %>% 
+#   lapply(read_csv) %>% 
+#   bind_rows 
+# 
+# de.2. <- list.files(pattern = "de.2.csv", recursive = TRUE) %>% 
+#   lapply(read_csv) %>% 
+#   bind_rows 
+
+superlig <- list.files(pattern = "1-superlig.csv", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+sco.1. <- list.files(pattern = "sco.1.csv", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+sco.2. <- list.files(pattern = "sco.2.csv", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+sco.3. <- list.files(pattern = "sco.3.csv", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+sco.4. <- list.files(pattern = "sco.4.csv", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+sco <- list.files(pattern = ".*(sco).*csv$", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+liga <- list.files(pattern = "1-liga.csv", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+seriea <- list.files(pattern = "-seriea.csv", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+serieb <- list.files(pattern = "1-serieb.csv", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+italy <- list.files(pattern = ".*(serie).*csv$", recursive = TRUE) %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+zbior <- X1_seriea
 #a. dominujący końcowy wynik spotkania (FT) i wynik pierwszej połowy (HT),
-sort_FT <-data.frame(summarise(group_by(eng.1.,FT),count =n()))
-sort_FT <- sort_FT[order(-sort_FT$count),]
+sort_FT <-data.frame(summarise(group_by(zbior,FT),count =n()))
+a <- sort_FT[order(-sort_FT$count),]
 #wynik drugiej połowy
-sort_HT <-data.frame(summarise(group_by(eng.1.,HT),count =n()))
-sort_HT <- sort_HT[order(-sort_HT$count),]
+sort_HT <-data.frame(summarise(group_by(zbior,HT),count =n()))
+a.1 <- sort_HT[order(-sort_HT$count),]
 
 #b. (jeśli jest jeden taki zespół) zespół najczęściej wygrywający spotkania,
 
 #divide string FT 
-out <- strsplit(as.character(eng.1.$FT),'-') 
+out <- strsplit(as.character(zbior$FT),'-') 
 do.call(rbind, out)
-test <- data.frame(eng.1.$FT, do.call(rbind, out), stringsAsFactors = FALSE)
-eng.1. <- cbind(eng.1., test[, 2:3])
+test <- data.frame(zbior$FT, do.call(rbind, out), stringsAsFactors = FALSE)
+zbior <- cbind(zbior, test[, 2:3])
 
 #removing charachtr after last space
-eng.1.$`Team 1` <- str_replace(eng.1.$`Team 1`, " \\(.*\\)", "")
-eng.1.$`Team 2` <- str_replace(eng.1.$`Team 2`, " \\(.*\\)", "")
-#test$`Team 2` <- str_replace(eng.1.$`Team 2`, " \\(.*\\)", "")
+zbior$`Team 1` <- str_replace(zbior$`Team 1`, " \\(.*\\)", "")
+zbior$`Team 2` <- str_replace(zbior$`Team 2`, " \\(.*\\)", "")
+#test$`Team 2` <- str_replace(zbior$`Team 2`, " \\(.*\\)", "")
 
-wynik <- ifelse(eng.1.$X1 > eng.1.$X2, eng.1.$`Team 2`,
-       ifelse(eng.1.$X1 < eng.1.$X2,eng.1.$`Team 1`,'draw'))
+wynik <- ifelse(zbior$X1 > zbior$X2, zbior$`Team 1`, 
+              ifelse(zbior$X1 < zbior$X2, zbior$`Team 2`, 
+                ifelse(zbior$X1 == zbior$X2, "draw", "draw")
+       )
+)
+
 wynik <- data.frame(wynik, stringsAsFactors = FALSE)
 sort_most_win <- as.data.frame(table(unlist(wynik)))
-sort_most_win <- sort_most_win[order(-sort_most_win$Freq),]
+b <- sort_most_win[order(-sort_most_win$Freq),]
 
 
 
 #c. (jeśli jest jeden taki zespół) zespół najczęściej wygrywający po pierwszej połowie,
 #divide string FT 
-out_ht <- strsplit(as.character(eng.1.$HT),'-') 
+out_ht <- strsplit(as.character(zbior$HT),'-') 
 do.call(rbind, out_ht)
-test_ht <- data.frame(eng.1.$HT, do.call(rbind, out_ht), stringsAsFactors = FALSE)
-eng.1. <- cbind(eng.1., test_ht[, 2:3])
-colnames(eng.1.)[10] <- "X4"
-colnames(eng.1.)[9] <- "X3"
+test_ht <- data.frame(zbior$HT, do.call(rbind, out_ht), stringsAsFactors = FALSE)
+zbior <- cbind(zbior, test_ht[, 2:3])
+colnames(zbior)[10] <- "X4"
+colnames(zbior)[9] <- "X3"
 
 #removing charachtr after last space
-#eng.1.$`Team 1` <- str_replace(eng.1.$`Team 1`, " \\(.*\\)", "")
-#eng.1.$`Team 2` <- str_replace(eng.1.$`Team 2`, " \\(.*\\)", "")
-#test$`Team 2` <- str_replace(eng.1.$`Team 2`, " \\(.*\\)", "")
+#zbior$`Team 1` <- str_replace(zbior$`Team 1`, " \\(.*\\)", "")
+#zbior$`Team 2` <- str_replace(zbior$`Team 2`, " \\(.*\\)", "")
+#test$`Team 2` <- str_replace(zbior$`Team 2`, " \\(.*\\)", "")
 
-wynik_ht <- ifelse(eng.1.$X3 > eng.1.$X4, eng.1.$`Team 2`,
-            ifelse(eng.1.$X3 < eng.1.$X4,eng.1.$`Team 1`,'draw'))
+wynik_ht <- ifelse(zbior$X3 > zbior$X4, zbior$`Team 2`,
+            ifelse(zbior$X3 < zbior$X4,zbior$`Team 1`,'draw'))
 wynik_ht <- data.frame(wynik_ht, stringsAsFactors = FALSE)
 sort_most_win <- as.data.frame(table(unlist(wynik_ht)))
-sort_most_win <- sort_most_win[order(-sort_most_win$Freq),]
+c <- sort_most_win[order(-sort_most_win$Freq),]
 
 #d. (jeśli jest jeden taki zespół) zespół, 
 #który uzyskał największą przewagę punktową 
 #(liczoną jako różnica w liczbie goli w ciągu jednego spotkania) nad przeciwnikiem w historii rozgrywek, dzień tygodnia i datę rozegrania tego spotkania.
 
-eng.1.$X1 <- as.numeric(eng.1.$X1)
-eng.1.$X2 <- as.numeric(eng.1.$X2)
+zbior$X1 <- as.numeric(zbior$X1)
+zbior$X2 <- as.numeric(zbior$X2)
 
+przewaga <- zbior$X1 - zbior$X2
+d <- data.frame(przewaga, zbior$Date, 
+                       zbior$`Team 1`, zbior$`Team 2`)
+d$przewaga = as.numeric(gsub("\\-", "", d$przewaga))
 
-prezwaga <- eng.1.$X1 - eng.1.$X2
-przewaga <- data.frame(prezwaga, eng.1.$Date, 
-                       eng.1.$`Team 1`, eng.1.$`Team 2`)
 
 #e. (jeśli jest jeden) zespół, który rozegrał najwięcej spotkań w historii rozgrywek,
 library(dplyr)
-df2 <- data.frame(a = c(eng.1.[,"Team 1"], eng.1.[,"Team 2"]))
+df2 <- data.frame(a = c(zbior[,"Team 1"], zbior[,"Team 2"]))
 sort_most_win <- as.data.frame(table(unlist(df2)))
-sort_most_win <- sort_most_win[order(-sort_most_win$Freq),]
+e <- sort_most_win[order(-sort_most_win$Freq),]
